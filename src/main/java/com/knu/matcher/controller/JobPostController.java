f36f2b16f6178ba9fc4fd5844480202242cd5342
Hong <bayyyy@naver.com>
2023-11-21 22:25:02 +0900

    [Feat]: JobPostController 구인구직 생성,수정,삭제 & 댓글 생성,삭제 구현

--- src/main/java/com/knu/matcher/controller/JobPostController.java
+++ src/main/java/com/knu/matcher/controller/JobPostController.java
@@ -1,4 +1,45 @@
 package com.knu.matcher.controller;
 
+import com.knu.matcher.dto.jobpost.CreateCommentRequest;
+import com.knu.matcher.dto.jobpost.CreateJobPostRequest;
+import com.knu.matcher.dto.jobpost.EditJobPostRequest;
+import com.knu.matcher.service.JobPostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/jobpost")
 public class JobPostController {
+    private final JobPostService jobPostService;
+
+    @GetMapping
+    public Long createJobPost(@RequestBody CreateJobPostRequest dto) {
+        String userEmail = "user2@example.com";
+        return jobPostService.createJobPost(userEmail, dto);
+    }
+
+    @PutMapping("/{jid}")
+    public void updateJobPost(@PathVariable("jid") Long jid, @RequestBody EditJobPostRequest dto) {
+        String userEmail = "user2@example.com";
+        jobPostService.updateJobPost(userEmail, jid, dto);
+    }
+
+    @DeleteMapping("/{jid}")
+    public void deleteJobPost(@PathVariable("jid") Long jid) {
+        String userEmail = "user2@example.com";
+        jobPostService.deleteJobPost(userEmail, jid);
+    }
+
+    @PostMapping("/{jid}/comment")
+    public Long createComment(@PathVariable("jid") Long jid, @RequestBody CreateCommentRequest dto) {
+        String userEmail = "user2@example.com";
+        return jobPostService.createComment(userEmail, jid, dto);
+    }
+    @DeleteMapping("/{jid}/comment/{cid}")
+    public void deleteComment(@PathVariable("jid") Long jid, @PathVariable("cid") Long cid) {
+        String userEmail = "user2@example.com";
+        jobPostService.deleteComment(userEmail, jid, cid);
+    }
+
 }
