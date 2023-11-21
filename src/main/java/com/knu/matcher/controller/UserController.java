e9fce721ad842d54afdabf942bccb20ebb5e4543
sim-mer <gyu2781@gmail.com>
2023-11-21 10:58:00 +0900

    [Feat] : 컨트롤러 레이어 - 마이페이지 작성

---
+++ src/main/java/com/knu/matcher/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.knu.matcher.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public UserDto getUser() {
+        return userService.getUser();
+    }
+
+    @PutMapping
+    public void updateUser(@RequestBody EditUserDto dto) {
+        userService.updateUser(dto);
+    }
+
+    @DeleteMapping
+    public void deleteUser() {
+        userService.deleteUser();
+    }
+}
