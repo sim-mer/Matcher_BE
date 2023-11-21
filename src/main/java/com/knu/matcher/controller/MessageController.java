7eccb7928a70e604756fe7d80a418090122aafbb
sim-mer <gyu2781@gmail.com>
2023-11-21 10:58:10 +0900

    [Feat] : 컨트롤러 레이어 - 메시지 기능 작성

--- src/main/java/com/knu/matcher/controller/MessageController.java
+++ src/main/java/com/knu/matcher/controller/MessageController.java
@@ -1,4 +1,36 @@
 package com.knu.matcher.controller;
 
+import com.knu.matcher.dto.request.CreateMessageDto;
+import com.knu.matcher.dto.response.message.MessageDetailDto;
+import com.knu.matcher.dto.response.message.MessageListDto;
+import com.knu.matcher.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/message")
 public class MessageController {
+    private MessageService messageService;
+
+    @PostMapping
+    public long createMessage(@RequestBody CreateMessageDto dto) {
+        return messageService.createMessage(dto);
+    }
+
+    @GetMapping("/{userEmail}")
+    public MessageDetailDto getMessage(@PathVariable String userEmail) {
+        return messageService.getMessage(userEmail);
+    }
+
+    @GetMapping
+    public MessageListDto getMessages() {
+        return messageService.getMessages();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMessage(@PathVariable long id) {
+        messageService.deleteMessage(id);
+    }
+
 }
