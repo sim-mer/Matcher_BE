f84e7db94e7a872fdb30ccf0ccc8eb58951b31f9
Hong <bayyyy@naver.com>
2023-11-21 17:25:43 +0900

    [Feat]: 미구현파일 주석

--- src/main/java/com/knu/matcher/controller/ReservationPostController.java
+++ src/main/java/com/knu/matcher/controller/ReservationPostController.java
@@ -1,42 +1,42 @@
-package com.knu.matcher.controller;
-
-import com.knu.matcher.dto.request.CreateReservationPostDto;
-import com.knu.matcher.dto.request.EditReservationPostDto;
-import com.knu.matcher.dto.response.reservation.ReservationPostDetailDto;
-import com.knu.matcher.dto.response.reservation.ReservationPostPagingDto;
-import com.knu.matcher.service.ReservationPostService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/reservationpost")
-public class ReservationPostController {
-    private final ReservationPostService reservationPostService;
-
-    @PostMapping
-    public long createReservationPost(@RequestBody CreateReservationPostDto dto) {
-        return reservationPostService.createReservationPost(dto);
-    }
-
-    @PutMapping("/{id}")
-    public void updateReservationPost(@PathVariable long id, @RequestBody EditReservationPostDto dto) {
-        reservationPostService.updateReservationPost(id, dto);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteReservationPost(@PathVariable long id) {
-        reservationPostService.deleteReservationPost(id);
-    }
-
-    @GetMapping("/{id}")
-    public ReservationPostDetailDto getReservationPost(@PathVariable long id) {
-        return reservationPostService.getReservationPostDetail(id);
-    }
-
-    @GetMapping
-    public ReservationPostPagingDto getReservationPosts(@RequestParam int page, @RequestParam(required = false) String title) {
-        return reservationPostService.getReservationPosts(page, title);
-    }
-
-}
+//package com.knu.matcher.controller;
+//
+//import com.knu.matcher.dto.request.CreateReservationPostDto;
+//import com.knu.matcher.dto.request.EditReservationPostDto;
+//import com.knu.matcher.dto.response.reservation.ReservationPostDetailDto;
+//import com.knu.matcher.dto.response.reservation.ReservationPostPagingDto;
+//import com.knu.matcher.service.ReservationPostService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/reservationpost")
+//public class ReservationPostController {
+//    private final ReservationPostService reservationPostService;
+//
+//    @PostMapping
+//    public long createReservationPost(@RequestBody CreateReservationPostDto dto) {
+//        return reservationPostService.createReservationPost(dto);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public void updateReservationPost(@PathVariable long id, @RequestBody EditReservationPostDto dto) {
+//        reservationPostService.updateReservationPost(id, dto);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void deleteReservationPost(@PathVariable long id) {
+//        reservationPostService.deleteReservationPost(id);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ReservationPostDetailDto getReservationPost(@PathVariable long id) {
+//        return reservationPostService.getReservationPostDetail(id);
+//    }
+//
+//    @GetMapping
+//    public ReservationPostPagingDto getReservationPosts(@RequestParam int page, @RequestParam(required = false) String title) {
+//        return reservationPostService.getReservationPosts(page, title);
+//    }
+//
+//}
