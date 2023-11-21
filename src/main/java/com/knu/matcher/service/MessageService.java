09ae636791954f3bee28abdfc3c1a36dfb45c2a2
Hong <bayyyy@naver.com>
2023-11-21 19:54:14 +0900

    [Feat]: message 기능 구현

--- src/main/java/com/knu/matcher/service/MessageService.java
+++ src/main/java/com/knu/matcher/service/MessageService.java
@@ -1,4 +1,62 @@
 package com.knu.matcher.service;
 
+import com.knu.matcher.domain.message.Message;
+import com.knu.matcher.domain.user.User;
+import com.knu.matcher.dto.message.CreateMessageRequest;
+import com.knu.matcher.dto.message.MessageDetailDto;
+import com.knu.matcher.dto.message.MessageSummaryDto;
+import com.knu.matcher.repository.MessageRepository;
+import com.knu.matcher.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class MessageService {
+    private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
+
+
+    public Long createMessage(String userEmail, CreateMessageRequest dto) {
+        Long id = messageRepository.findLastMessageId() + 1;
+        Message message = Message.builder()
+                .id(id)
+                .content(dto.getContent())
+                .date(LocalDateTime.now())
+                .senderEmail(userEmail)
+                .receiverEmail(dto.getReceiverEmail())
+                .build();
+        return messageRepository.save(message);
+    }
+    public List<MessageSummaryDto> getMessagesSummary(String userEmail) {
+        List<String> emails = messageRepository.findUsersEmailInChatByUserEmail(userEmail);
+        return emails.stream().map(email ->{
+            User user = userRepository.findById(email);
+            String userName = user.getName();
+            MessageSummaryDto messageSummaryDto = MessageSummaryDto.builder()
+                    .userEmail(email)
+                    .userName(userName)
+                    .build();
+            return messageSummaryDto;
+        }).collect(Collectors.toList());
+    }
+
+    public List<MessageDetailDto> getMessagesDetail(String userEmail, String otherUserEmail) {
+        List<Message> messageList = messageRepository.findMessagesByEmail(userEmail,otherUserEmail);
+
+        return messageList.stream().map(MessageDetailDto::fromDomain).collect(Collectors.toList());
+    }
+
+
+    public void deleteMessage(String userEmail, long mid) {
+        Message message = messageRepository.findById(mid);
+        if(!message.getSenderEmail().equals(userEmail)){
+            throw new IllegalArgumentException("해당 메시지를 삭제할 권한이 없습니다.");
+        }
+        messageRepository.delete(mid);
+    }
 }
