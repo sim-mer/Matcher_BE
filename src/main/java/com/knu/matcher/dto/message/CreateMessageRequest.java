75e51d6a0f635fba802977e77d25f4af0865aed3
Hong <bayyyy@naver.com>
2023-11-21 20:57:29 +0900

    [Feat]: message validation 추가

--- src/main/java/com/knu/matcher/dto/message/CreateMessageRequest.java
+++ src/main/java/com/knu/matcher/dto/message/CreateMessageRequest.java
@@ -2,8 +2,14 @@ package com.knu.matcher.dto.message;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 public class CreateMessageRequest {
+    @Email(message = "이메일 형식으로 입력해주세요")
     private String receiverEmail;
+
+    @NotEmpty(message = "내용을 입력해주세요")
     private String content;
 }
