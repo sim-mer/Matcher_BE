67201d43bb85b762ce72c53d2de6dab9c554f571
Hong <bayyyy@naver.com>
2023-11-22 18:56:44 +0900

    [Feat]: jwtToken Resolver 추가

--- src/main/java/com/knu/matcher/web/WebConfig.java
+++ src/main/java/com/knu/matcher/web/WebConfig.java
@@ -1,14 +1,21 @@
 package com.knu.matcher.web;
 
+import com.knu.matcher.handler.TokenUserEmailResolver;
+import com.knu.matcher.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
-public class WebConfig {
-
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final JwtUtil jwtUtil;
     @Bean
     public FilterRegistrationBean tokenCheckFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
@@ -16,5 +23,10 @@ public class WebConfig {
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new TokenUserEmailResolver(jwtUtil));
     }
 }
