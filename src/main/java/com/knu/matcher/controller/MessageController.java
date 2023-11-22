e1d44f0253826e05d49a694facce2b11318c9e3f
Hong <bayyyy@naver.com>
2023-11-22 18:58:20 +0900

    [Feat]: MessageController jwt 적용

--- src/main/java/com/knu/matcher/controller/MessageController.java
+++ src/main/java/com/knu/matcher/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.knu.matcher.controller;
 
+import com.knu.matcher.annotation.TokenUserEmail;
 import com.knu.matcher.dto.message.CreateMessageRequest;
 import com.knu.matcher.dto.message.MessageDetailDto;
 import com.knu.matcher.dto.message.MessageSummaryDto;
@@ -8,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
@@ -19,28 +18,25 @@ public class MessageController {
     private final MessageService messageService;
 
     @PostMapping
-    public Long createMessage(@RequestBody @Valid CreateMessageRequest dto) {
-        String userEmail = "user2@example.com";
+    public Long createMessage(@RequestBody @Valid CreateMessageRequest dto,@TokenUserEmail String userEmail) {
         return messageService.createMessage(userEmail, dto);
     }
 
 
     @GetMapping
-    public List<MessageSummaryDto> getMessagesSummary() {
-        String userEmail = "user2@example.com";
+    public List<MessageSummaryDto> getMessagesSummary(@TokenUserEmail String userEmail) {
+        System.out.println(userEmail);
 
         return messageService.getMessagesSummary(userEmail);
     }
 
     @GetMapping("/{otherUserEmail}")
-    public List<MessageDetailDto> getMessagesDetail(@PathVariable String otherUserEmail) {
-        String userEmail = "user2@example.com";
+    public List<MessageDetailDto> getMessagesDetail(@PathVariable String otherUserEmail, @TokenUserEmail String userEmail) {
         return messageService.getMessagesDetail(userEmail, otherUserEmail);
     }
 
     @DeleteMapping("/{mid}")
-    public void deleteMessage(@PathVariable long mid) {
-        String userEmail = "user2@example.com";
+    public void deleteMessage(@PathVariable long mid, @TokenUserEmail String userEmail) {
         messageService.deleteMessage(userEmail, mid);
     }
 
