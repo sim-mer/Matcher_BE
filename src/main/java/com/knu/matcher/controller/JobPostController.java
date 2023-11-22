b11695357baeea1ee0d8f384e8e522aed4550a3f
Hong <bayyyy@naver.com>
2023-11-22 19:00:12 +0900

    [Feat]: JobPostController jwt 적용

--- src/main/java/com/knu/matcher/controller/JobPostController.java
+++ src/main/java/com/knu/matcher/controller/JobPostController.java
@@ -1,5 +1,6 @@
 package com.knu.matcher.controller;
 
+import com.knu.matcher.annotation.TokenUserEmail;
 import com.knu.matcher.dto.common.OffsetPagingResponse;
 import com.knu.matcher.dto.jobpost.*;
 import com.knu.matcher.service.JobPostService;
@@ -23,31 +24,26 @@ public class JobPostController {
     }
 
     @PostMapping
-    public Long createJobPost(@RequestBody CreateJobPostRequest dto) {
-        String userEmail = "user2@example.com";
+    public Long createJobPost(@RequestBody CreateJobPostRequest dto, @TokenUserEmail String userEmail) {
         return jobPostService.createJobPost(userEmail, dto);
     }
 
     @PutMapping("/{jid}")
-    public void updateJobPost(@PathVariable("jid") Long jid, @RequestBody EditJobPostRequest dto) {
-        String userEmail = "user2@example.com";
+    public void updateJobPost(@PathVariable("jid") Long jid, @RequestBody EditJobPostRequest dto, @TokenUserEmail String userEmail) {
         jobPostService.updateJobPost(userEmail, jid, dto);
     }
 
     @DeleteMapping("/{jid}")
-    public void deleteJobPost(@PathVariable("jid") Long jid) {
-        String userEmail = "user2@example.com";
+    public void deleteJobPost(@PathVariable("jid") Long jid, @TokenUserEmail String userEmail) {
         jobPostService.deleteJobPost(userEmail, jid);
     }
 
     @PostMapping("/{jid}/comment")
-    public Long createComment(@PathVariable("jid") Long jid, @RequestBody CreateCommentRequest dto) {
-        String userEmail = "user2@example.com";
+    public Long createComment(@PathVariable("jid") Long jid, @RequestBody CreateCommentRequest dto, @TokenUserEmail String userEmail) {
         return jobPostService.createComment(userEmail, jid, dto);
     }
     @DeleteMapping("/{jid}/comment/{cid}")
-    public void deleteComment(@PathVariable("jid") Long jid, @PathVariable("cid") Long cid) {
-        String userEmail = "user2@example.com";
+    public void deleteComment(@PathVariable("jid") Long jid, @PathVariable("cid") Long cid, @TokenUserEmail String userEmail) {
         jobPostService.deleteComment(userEmail, jid, cid);
     }
 
