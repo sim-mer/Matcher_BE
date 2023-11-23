6e147e367633b41712e0f1acb84dbb0ccb612869
Gyumin Sim <gyu2781@gmail.com>
2023-11-23 23:04:43 +0900

    [Feat] : 예약 게시글 목록 조회 DTO 수정

--- src/main/java/com/knu/matcher/dto/response/reservation/ReservationPostPagingDto.java
+++ src/main/java/com/knu/matcher/dto/response/reservation/ReservationPostPagingDto.java
@@ -1,21 +1,16 @@
 package com.knu.matcher.dto.response.reservation;
 
+
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
+@Data
+@Builder
 public class ReservationPostPagingDto {
-    boolean hasNext;
-    List<ReservationPost> data;
-
-    @Data
-    @NoArgsConstructor
-    public class ReservationPost {
-        long id;
-        String title;
-        LocalDateTime date;
-        AuthorDto author;
-    }
+    long id;
+    String title;
+    LocalDateTime date;
+    String ownerName;
 }
