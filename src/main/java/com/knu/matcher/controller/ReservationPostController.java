a0ca4cb1b80cfe0d4fff628c697cd0fbc28f353c
sim-mer <gyu2781@gmail.com>
2023-11-23 11:15:09 +0900

    [Feat] : 컨트롤러 레이어 - 예약 생성, 수정, 삭제 기능 작

--- src/main/java/com/knu/matcher/controller/ReservationPostController.java
+++ src/main/java/com/knu/matcher/controller/ReservationPostController.java
@@ -1,33 +1,34 @@
-//package com.knu.matcher.controller;
-//
-//import com.knu.matcher.dto.request.CreateReservationPostDto;
-//import com.knu.matcher.dto.request.EditReservationPostDto;
-//import com.knu.matcher.dto.response.reservation.ReservationPostDetailDto;
-//import com.knu.matcher.dto.response.reservation.ReservationPostPagingDto;
-//import com.knu.matcher.service.ReservationPostService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/reservationpost")
-//public class ReservationPostController {
-//    private final ReservationPostService reservationPostService;
-//
-//    @PostMapping
-//    public long createReservationPost(@RequestBody CreateReservationPostDto dto) {
-//        return reservationPostService.createReservationPost(dto);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public void updateReservationPost(@PathVariable long id, @RequestBody EditReservationPostDto dto) {
-//        reservationPostService.updateReservationPost(id, dto);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void deleteReservationPost(@PathVariable long id) {
-//        reservationPostService.deleteReservationPost(id);
-//    }
+package com.knu.matcher.controller;
+
+import com.knu.matcher.annotation.TokenUserEmail;
+import com.knu.matcher.dto.request.CreateReservationPostDto;
+import com.knu.matcher.dto.request.EditReservationPostDto;
+import com.knu.matcher.dto.response.reservation.ReservationPostDetailDto;
+import com.knu.matcher.dto.response.reservation.ReservationPostPagingDto;
+import com.knu.matcher.service.ReservationPostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reservationpost")
+public class ReservationPostController {
+    private final ReservationPostService reservationPostService;
+
+    @PostMapping
+    public long createReservationPost(@RequestBody CreateReservationPostDto dto, @TokenUserEmail String email) {
+        return reservationPostService.createReservationPost(dto, email);
+    }
+
+    @PutMapping("/{id}")
+    public void updateReservationPost(@PathVariable long id, @RequestBody EditReservationPostDto dto) {
+        reservationPostService.updateReservationPost(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReservationPost(@PathVariable long id) {
+        reservationPostService.deleteReservationPost(id);
+    }
 //
 //    @GetMapping("/{id}")
 //    public ReservationPostDetailDto getReservationPost(@PathVariable long id) {
@@ -38,5 +39,5 @@
 //    public ReservationPostPagingDto getReservationPosts(@RequestParam int page, @RequestParam(required = false) String title) {
 //        return reservationPostService.getReservationPosts(page, title);
 //    }
-//
-//}
+
+}
